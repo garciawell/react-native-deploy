32d54b32cf095aa315f36944c648d5d689330711
Wellington Garcia <garciawell@gmail.com>
2020-02-20 19:51:10 -0300

    Style app

---
+++ styled.js
@@ -0,0 +1,15 @@
+import styled from 'styled-components/native';
+
+export const Container = styled.View`
+  flex: 1;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  background: #2e064f;
+`;
+
+export const Title = styled.Text`
+  color: #fff;
+  font-size: 48px;
+  font-weight: bold;
+`;
