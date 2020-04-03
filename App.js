36992790e8d9da0a04148559c5215e8640ac44cb
Wellington Garcia <garciawell@gmail.com>
2020-04-03 15:40:08 -0300

    alterando config codepush

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import {StatusBar} from 'react-native';
+import {StatusBar, TextInput} from 'react-native';
 import {Container, Title} from './styled';
 import codePush from 'react-native-code-push';
 
@@ -8,12 +8,13 @@ const App = () => {
     <>
       <StatusBar barStyle="dark-content" />
       <Container>
-        <Title>Spoti Light (WG)</Title>
+        <Title>Spoti Light V3</Title>
+        <TextInput />
       </Container>
     </>
   );
 };
 
-let codePushOptions = {checkFrequency: codePush.CheckFrequency.ON_APP_RESUME};
+let codePushOptions = {checkFrequency: codePush.CheckFrequency.ON_APP_START};
 
 export default codePush(codePushOptions)(App);
